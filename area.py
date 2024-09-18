438f0b4a88d324fe1a3b4cd6c37a55c107f51d4d
ajmainwasif01 <nipunazmun@gmail.com>
2024-09-18 17:29:51 +0600

    Area Calculator

---
+++ area.py
@@ -0,0 +1,4 @@
+Base = int(input("Enter the value of base:"))
+Height = int(input("Enter the value of height:"))
+Area = 1/2* Base * Height
+print("Result:" , Area)
---
+++ circlearea.py
@@ -0,0 +1,3 @@
+Radius = int(input("Enter The Value Of Radius:"))
+Area = 3.1416 * Radius**2
+print("Result:" , Area)
